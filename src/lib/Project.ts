7d362e23f26742af7ca0df51b1b33bd6d6046b34
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2023-01-10 15:03:31 +0300

    make project lazy-loads gas tanks

--- src/lib/Project.ts
+++ src/lib/Project.ts
@@ -2,6 +2,8 @@ import { Pool } from 'pg';
 
 import {
     addGasTankQuery,
+    addMultiGasTankWhitelistQuery,
+    getGasTankByChainIdQuery,
     getGasTanksByProjectIdQuery
 } from '../constants/database';
 import { GasTankProps, GasTanksType } from '../types';
@@ -18,16 +20,29 @@ export default class Project {
     owner?: string;
     createdAt?: Date;
     allowedOrigins?: Array<string>;
+    name?: string;
+    #loadAllOnInit?: boolean;
 
-    constructor(projectApiKey: string, pool: Pool, authToken: string) {
+    constructor(
+        projectApiKey: string,
+        pool: Pool,
+        authToken: string,
+        loadAllOnInit?: boolean
+    ) {
         this.#projectApiKey = projectApiKey;
         this.#authToken = authToken;
         this.#pool = pool;
+        this.#loadAllOnInit = loadAllOnInit;
 
-        this.readyPromise = this.init();
+        this.readyPromise = loadAllOnInit
+            ? this.initWithGasTanks()
+            : this.initProjectDetails();
     }
 
-    async addGasTank(gasTank: Omit<GasTankProps, 'apiKey'>): Promise<void> {
+    async addGasTank(
+        gasTank: Omit<GasTankProps, 'apiKey' | 'createdAt'>,
+        whiteList: string[]
+    ): Promise<void> {
         await this.readyPromise;
         if (this.#gasTanks[gasTank.name] !== undefined) {
             throw new Error('gas tank name should be unique');
@@ -38,23 +53,34 @@ export default class Project {
             this.#authToken
         );
         const now = new Date();
-        await this.#pool.query(addGasTankQuery, [
-            apiKey,
-            this.#projectApiKey,
-            now,
-            gasTank.name,
-            gasTank.chainId,
-            gasTank.providerURL,
-            fundingKey
+        const gasTankId = (
+            await this.#pool.query(addGasTankQuery, [
+                apiKey,
+                this.#projectApiKey,
+                now,
+                gasTank.name,
+                gasTank.chainId,
+                gasTank.providerURL,
+                fundingKey
+            ])
+        ).rows[0].gas_tank_id;
+
+        const gasTankIdList = Array(whiteList.length).fill(gasTankId);
+        await this.#pool.query(addMultiGasTankWhitelistQuery, [
+            whiteList,
+            gasTankIdList
         ]);
 
-        this.#gasTanks[gasTank.name] = new GasTank(
-            {
-                apiKey,
-                ...gasTank
-            },
-            this.#pool
-        );
+        if (this.#loadAllOnInit) {
+            this.#gasTanks[gasTank.name] = new GasTank(
+                {
+                    apiKey,
+                    ...gasTank,
+                    createdAt: now.toDateString()
+                },
+                this.#pool
+            );
+        }
     }
 
     async initProjectDetails(): Promise<void> {
@@ -68,39 +94,67 @@ export default class Project {
         this.owner = res.rows[0].owner_scw;
         this.createdAt = res.rows[0].created_at;
         this.allowedOrigins = res.rows[0].allowed_origins;
+        this.name = res.rows[0].name;
     }
 
     async initGasTanks(): Promise<void> {
-        const gasTanks: GasTanksType = await this.obtainGasTanksFromDatabase();
-        console.log('gasTanks', gasTanks);
+        const gasTanks: GasTanksType = await this.#obtainGasTanksFromDatabase();
         if (gasTanks) {
-            gasTanks.forEach((gasTank: GasTankProps) => {
-                if (this.#gasTanks[gasTank.name] !== undefined) {
-                    throw new Error('gas tank name should be unique');
-                }
-                this.#gasTanks[gasTank.name] = new GasTank(gasTank, this.#pool);
-            });
+            await Promise.all(
+                gasTanks.map(async (gasTank: GasTankProps) => {
+                    if (this.#gasTanks[gasTank.name] !== undefined) {
+                        throw new Error('gas tank name should be unique');
+                    }
+                    this.#gasTanks[gasTank.name] = new GasTank(
+                        gasTank,
+                        this.#pool
+                    );
+                    await this.#gasTanks[gasTank.name].readyPromise;
+                })
+            );
         }
     }
 
-    async init(): Promise<void> {
+    async initWithGasTanks(): Promise<void> {
         await Promise.all([this.initProjectDetails(), this.initGasTanks()]);
     }
 
-    async obtainGasTanksFromDatabase(): Promise<GasTanksType> {
+    async #obtainGasTanksFromDatabase(): Promise<GasTanksType> {
         try {
             const res = await this.#pool.query<GasTankProps>(
                 getGasTanksByProjectIdQuery,
                 [this.#projectApiKey]
             );
-            console.log(res.rows);
-            return res.rows;
+            return res.rows.map((gasTank: GasTankProps) => ({
+                ...gasTank,
+                fundingKey: +gasTank.fundingKey
+            }));
         } catch (err) {
             throw new Error(err as string);
         }
     }
 
-    getGasTank(name: string): GasTank {
+    async loadAndGetGasTankByChainId(chainId: number): Promise<GasTank> {
+        const { rows } = await this.#pool.query<GasTankProps>(
+            getGasTankByChainIdQuery,
+            [this.#projectApiKey, chainId]
+        );
+        if (rows.length === 0) {
+            throw new Error('gas tank not found');
+        }
+        const gasTankProps = {
+            ...rows[0],
+            fundingKey: +rows[0].fundingKey
+        };
+        const gasTank = new GasTank(gasTankProps, this.#pool);
+        return gasTank;
+    }
+
+    getLoadedGasTank(name: string): GasTank {
         return this.#gasTanks[name];
+    }
+
+    public get apiKey(): string {
+        return this.#projectApiKey;
     }
 }
