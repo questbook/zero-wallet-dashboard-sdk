bf415a2cde190b5d0cbc63b25a1192405cb6fa39
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2023-03-15 22:57:00 +0300

    fix: export supported chain ids

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import { SupportedChainId } from './constants/chains'
 import Project from './lib/Project';
 import ProjectsManager from './lib/ProjectsManager';
 import { configEnv } from './utils/global';
 
 configEnv();
 
-export { Project, ProjectsManager };
+export { Project, ProjectsManager, SupportedChainId };
 export * from './types';
