1ae7b5fc29ca88026c53c22b50a7738c71d4e7f3
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2023-03-15 20:09:01 +0300

    fix: remove deprecated example

--- example/src/example.ts
+++
@@ -1,9 +0,0 @@
-const Pool = require('pg').Pool;
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'test',
-    password: 'postgres',
-    port: 5432
-});
-pool.query("CREATE TABLE IF NOT EXISTS gasless_login (address VARCHAR(42) PRIMARY KEY, nonce VARCHAR(100), expiration BIGINT, gas_tank_id VARCHAR(42));");
